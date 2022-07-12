18db2b7a9f75eed2c94abfcbde8ff5c492c0911a
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-07-12 15:07:01 +0800

    Update vc_vpx.sh

--- vc_vpx.sh
+++ vc_vpx.sh
@@ -7,8 +7,9 @@ function _install_vpx() {
 	echo -e $RED"Installation of $_package ....... started"$RESET
 	cd $SOURCE_DIR/
 	[ -d "libvpx" ] && rm -rf "libvpx"
-	git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
-	cd libvpx
+	#git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+	tar -xvzf libvpx-1.10.0.tar.gz
+	cd libvpx-1.10.0
 	./configure --prefix=$INSTALL_DIR --enable-shared --enable-pic --disable-examples --disable-unit-tests
 	make -j $cpu
 	make install
