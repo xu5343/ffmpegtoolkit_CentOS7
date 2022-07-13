1c57021af1b10dafb5d875df98772850c4e45eb1
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-07-13 17:38:28 +0800

    Update vc_ffmpeg.sh

--- vc_ffmpeg.sh
+++ vc_ffmpeg.sh
@@ -7,8 +7,11 @@ function _install_ffmpeg() {
 	echo -e $RED"Installation of $_package ....... started"$RESET
 	cd $SOURCE_DIR/
 	rm -vrf ffmpeg*
-	git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
-	cd ffmpeg/
+	#git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
+	#cd ffmpeg/
+	wget http://ffmpeg.org/releases/ffmpeg-4.0.6.tar.xz
+	tar xvf ffmpeg-4.0.6.tar.xz
+	cd ffmpeg-4.0.6/
 	ldconfig
         export LD_LIBRARY_PATH=/usr/local/ffmpegtoolkit/lib:/usr/local/lib:/usr/lib:$LD_LIBRARY_PATH
         export LIBRARY_PATH=/usr/local/ffmpegtoolkit/lib:/usr/lib:/usr/local/lib:$LIBRARY_PATH
